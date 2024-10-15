322c81f976c25aa41d0e435322709c3717eb2e38
Abhimanyu Singh <127570024+Abhimanyu5654@users.noreply.github.com>
2024-10-16 01:04:07 +0530

    node

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express')
+require('dotenv').config();
 // const mongoose=require('mongoose')
 const bodyparser=require('body-parser')
 const connection=require('./db')
@@ -6,7 +7,7 @@ const cors=require('cors')
 const userRouter=require('./controler/controler')
 
 const app=express()
-const port=8888
+const port=process.env.PORT||8888
 connection()
 app.use(cors())
 app.use(express.json())
