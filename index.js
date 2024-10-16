4e7d3a42a6af069dc1a72eec77280f7671ecd424
Abhimanyu Singh <127570024+Abhimanyu5654@users.noreply.github.com>
2024-10-16 11:39:57 +0530

    node

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express')
 require('dotenv').config();
 // const mongoose=require('mongoose')
-const bodyparser=require('body-parser')
+const bodyParser=require('body-parser')
 const connection=require('./db')
 const cors=require('cors')
 const userRouter=require('./controler/controler')
@@ -11,8 +11,12 @@ const port=process.env.PORT||8888
 connection()
 app.use(cors())
 app.use(express.json())
-app.use(bodyparser.json())
-app.use(express.urlencoded({ extended: true }));
+// app.use(bodyparser.json()
+// app.use(express.urlencoded({ extended: true }));
+app.use( 
+    bodyParser.urlencoded({ 
+        extended: true}) 
+    ); 
 // app.use((req, res, next) => {
 //     res.locals.path = req.path;
 //     next();
